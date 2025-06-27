40ceedadea2ee9bb29bb82bf058898b59416c1a0
Arnoldrdh <arnoldrubben94@gmail.com>
2025-06-27 21:15:50 +0700

    added FK in migrations

---
+++ database/migrations/2025_06_27_133654_create_classrooms_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('classrooms', function (Blueprint $table) {
+            $table->id();
+            $table->integer('Grade');
+            $table->char('Code');
+            $table->integer('Year');
+            //referenre FK to user role teacher
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('classrooms');
+    }
+};
---
+++ database/migrations/2025_06_27_133656_create_reports_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Classroom;
+use App\Models\Period;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('period_id')->references('id')->on('perioda');
+            $table->foreignId('period_id')->references('id')->on('classrooms');
+            $table->foreignId('period_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reports');
+    }
+};
