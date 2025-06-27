80ad13e8d8053301794994bfa93ae516d5c4ddb9
Arnoldrdh <arnoldrubben94@gmail.com>
2025-06-27 22:34:43 +0700

    added jwt and implented it in AuthController and User model

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    //register
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'name' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        try {
+            $token = JWTAuth::fromUser($user);
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Could not create token'], 500);
+        }
+
+        return response()->json([
+            'token' => $token,
+            'user' => $user
+        ], 201);
+    }
+
+    //login
+    public function login(Request $request)
+    {
+        $credetentials = $request->only('email', 'password');
+
+        try {
+            if (!$token = JWTAuth::attempt($credetentials)) {
+                return response()->json(['error' => 'Invalid credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'could not create token'], 500);
+        }
+
+        return response()->json([
+            'token' => $token,
+            // 'expires_in' => auth('api')->factory()->getTTL() * 60,
+        ]);
+    }
+
+    //logout
+    public function logout()
+    {
+        try {
+            JWTAuth::invalidate(JWTAuth::getToken());
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Could not log out'], 500);
+        }
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+}
