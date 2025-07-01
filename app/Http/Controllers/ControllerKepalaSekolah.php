ee8e28db10288f6be0aedb1b7ec7afabbbcdbe51
vincent exelcio <vincent.celcio@gmail.com>
2025-07-01 19:04:43 +0700

    feat: make role nullable, add method to change role from kepala sekolah

--- app/Http/Controllers/ControllerKepalaSekolah.php
+++ app/Http/Controllers/ControllerKepalaSekolah.php
@@ -350,4 +350,79 @@ class ControllerKepalaSekolah extends Controller
 
         return response()->json(['message' => 'Periode berhasil dihapus']);
     }
+
+
+    public function updateMuridKelas(Request $request)
+    {
+        $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'new_class_id' => 'required|exists:classrooms,id',
+        ]);
+
+        $userId = $request->user_id;
+        $newClassId = $request->new_class_id;
+
+        // Ambil entri ClassStudent lama
+        $currentClassEntry = ClassStudent::where('user_id', $userId)->first();
+
+        // Jika tidak ada data sebelumnya
+        if (!$currentClassEntry) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Siswa belum terdaftar di kelas manapun',
+            ], 404);
+        }
+
+        // Cek apakah siswa sudah memiliki rapor di kelas saat ini
+        $hasReport = Report::where('user_id', $userId)
+            ->where('class_id', $currentClassEntry->classroom_id)
+            ->exists();
+
+        if ($hasReport) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Siswa sudah memiliki rapor di kelas saat ini dan tidak dapat dipindahkan',
+            ], 403);
+        }
+
+        // Update classroom_id ke kelas baru
+        $currentClassEntry->classroom_id = $newClassId;
+        $currentClassEntry->save();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Siswa berhasil dipindahkan ke kelas baru',
+        ], 200);
+    }
+
+
+    public function listTeacher()
+    {
+        $data = User::where('role', 'wali_kelas')->get();
+
+        return response()->json([
+            'message' => 'success',
+            'data' => $data
+        ], 200);
+    }
+
+    public function listUnvalidatedUser()
+    {
+        $data = User::where("role", null)->get();
+
+        return response()->json([
+            'message' => 'success',
+            'data' => $data
+        ], 200);
+    }
+
+    public function updateUser(Request $request, $id)
+    {
+        $data = User::findOrFail($id);
+        $data->update($request->only(['role']));
+        return response()->json([
+            'message' => 'success',
+            'data' => $data
+        ], 200);
+    }
 }
